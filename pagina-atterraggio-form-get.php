c8f2f8ebe481bbcb109e1bf3ddee1abf75737510
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-12 16:05:13 +0200

    [add] predisposto form con due input

--- pagina-atterraggio-form-get.php
+++ pagina-atterraggio-form-get.php
@@ -1,7 +1,8 @@
 <?php
+$nome = $_GET['paragrafo'];
+$nome = $_GET['parola'];
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,12 +11,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <title>PHP Badwords</title>
+    <title>Ricevo Form GET</title>
 </head>
 
 <body>
     <div class="container my-5">
-        <h1>Esercizio PHP</h1>
+        <p>Paragrafo: <php echo $paragrafo ?>
+        </p>
+        <h4>Parola da censurare: <php echo $parola ?>
+        </h4>
     </div>
 </body>
 
